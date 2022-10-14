0639718e5620b94238955cf0762f71bf327ce50e
reinerBa <4329883+reinerBa@users.noreply.github.com>
2022-10-14 23:35:12 +0200

    Hinzufügen von SRI-Sicherheitschecks für die verlinkten Scripte

--- jupo-webui/vite.config.ts
+++ jupo-webui/vite.config.ts
@@ -2,12 +2,18 @@ import { UserConfigExport, ConfigEnv } from 'vite'
 
 import { viteMockServe } from 'vite-plugin-mock'
 import vue from '@vitejs/plugin-vue'
+import sri from 'rollup-plugin-sri'
 
 export default ({ command }: ConfigEnv): UserConfigExport => {
   return {
+    base: "./",
     server: {open: true},
     plugins: [
       vue(),
+      {
+        enforce: "post",
+        ...sri({ publicPath: "./", selectors: ["script", "link[rel=modulepreload]"] })
+      },
       viteMockServe({
         // default
         mockPath: 'webapiMock',
